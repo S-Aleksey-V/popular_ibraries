22f3093fc2f07e7480ab808ac3f75b2a13f81143
Alexey <gamestol@mail.ru>
2021-03-02 20:38:45 +0300

    Initial commit

---
+++ app/src/main/java/me/tolkstudio/popularlibraries/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.tolkstudio.popularlibraries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
